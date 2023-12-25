1aa0575b77f46d088c5d229744c3653cdf28591a
rai <mj82xx@gmail.com>
2023-12-25 17:34:26 +0900

    chore: Astro MDX integrationを追加

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
 import solidJs from "@astrojs/solid-js"
+
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alg.tus-ricora.com/",
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), mdx()],
 })
