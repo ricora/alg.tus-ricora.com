d1bfa425676a0d89da04fe4069ea408d0979c08d
rai <mj82xx@gmail.com>
2023-12-26 13:13:43 +0900

    feat: remarkCalloutを作成

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-
 import mdx from "@astrojs/mdx"
+import { remarkCallout } from "./src/lib/remark-plugins/remarkCallout"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alg.tus-ricora.com/",
   integrations: [tailwind(), solidJs(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkCallout],
+  },
 })
