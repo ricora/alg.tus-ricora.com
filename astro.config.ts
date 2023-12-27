9ede470c8f7f7b17c9835c2203a6d7ccf5f6374b
rai <mj82xx@gmail.com>
2023-12-27 16:44:25 +0900

    chore(deps): リッチなCodeBlockの実装のためにrehype-custom-codeを導入

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,30 @@ import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 import mdx from "@astrojs/mdx"
 import { remarkCallout } from "./src/lib/remark-plugins/remarkCallout"
+import { rehypeCustomCode, type RehypeCustomCodeOptions } from "rehype-custom-code"
+import { remarkMetaString } from "remark-meta-string"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alg.tus-ricora.com/",
   integrations: [tailwind(), solidJs(), mdx()],
   markdown: {
-    remarkPlugins: [remarkCallout],
+    remarkPlugins: [remarkCallout, remarkMetaString],
+    rehypePlugins: [
+      [
+        rehypeCustomCode,
+        {
+          propsPrefix: "",
+          shouldExportCodeAsProps: true,
+          shiki: {
+            themes: {
+              light: "github-light",
+              dark: "one-dark-pro",
+            },
+          },
+        } satisfies RehypeCustomCodeOptions,
+      ],
+    ],
+    syntaxHighlight: false,
   },
 })
