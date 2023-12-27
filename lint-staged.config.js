538486d3ce9e205618b48487576e10ea8efb7adb
rai <96561881+r4ai@users.noreply.github.com>
2023-12-27 22:07:29 +0900

    chore: lint-stagedのPrettierの対象にMDXとCSSを追加 (#17)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
 /** @type {import("lint-staged").Config} */
 export default {
   "*.{js,cjs,ts,jsx,tsx,astro}": ["eslint --fix", "prettier --write"],
-  "*.{md,html,json,yaml,yml}": ["prettier --write"],
+  "*.{md,mdx,html,css,json,yaml,yml}": ["prettier --write"],
 }
