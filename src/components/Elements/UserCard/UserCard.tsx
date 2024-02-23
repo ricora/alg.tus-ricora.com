b3d4079967c0d282f63aed7e417c654e17a13b94
rai <mj82xx@gmail.com>
2024-02-23 11:45:51 +0900

    feat: NavBar関連のコンポーネントを、新たなデザインシステムに対応させる

---
+++ src/components/Elements/UserCard/UserCard.tsx
@@ -0,0 +1,46 @@
+import type { CollectionEntry } from "astro:content"
+import { Icon, type IconName } from "../Icon"
+import * as Tooltip from "@/components/ui/Tooltip"
+import { toUnicode } from "punycode"
+import type { Component } from "solid-js"
+
+const removeProtocol = (url: string) => {
+  const urlObj = new URL(url)
+  return toUnicode(urlObj.host) + decodeURI(urlObj.pathname)
+}
+
+type UserCardProps = CollectionEntry<"members">
+
+export const UserCard: Component<UserCardProps> = (props) => {
+  const user = props.data
+  return (
+    <div class="flex max-w-72 flex-col items-center gap-6 rounded-xl border bg-bg-default p-8">
+      <div>
+        <img class="rounded-full bg-white" width="120" height="120" src={user.image} alt={user.name} loading="lazy" />
+      </div>
+      <div class="text-2xl font-bold">{user.name}</div>
+      <div class="text-center text-sm text-fg-muted">{user.description}</div>
+      <div class="my-auto flex flex-row gap-4">
+        {user.social.map((s) => (
+          <Tooltip.Root>
+            <Tooltip.Trigger>
+              <a class="opacity-70 transition hover:opacity-100" href={s.link}>
+                {s.icon ? (
+                  <Icon name={s.icon as IconName} class="h-6 w-6" />
+                ) : (
+                  <Icon name="heroicons:link" class="h-6 w-6" />
+                )}
+              </a>
+            </Tooltip.Trigger>
+            <Tooltip.Positioner>
+              <Tooltip.Arrow>
+                <Tooltip.ArrowTip />
+              </Tooltip.Arrow>
+              <Tooltip.Content>{s.label ?? removeProtocol(s.link)}</Tooltip.Content>
+            </Tooltip.Positioner>
+          </Tooltip.Root>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/SideBar/NavBar/IconLink.tsx
@@ -0,0 +1,28 @@
+import { Icon, type IconName } from "@/components/Elements/Icon"
+import * as Tooltip from "@/components/ui/Tooltip"
+import { splitProps, type ComponentProps } from "solid-js"
+
+type Props = ComponentProps<"a"> & {
+  name: IconName
+  label: string
+}
+
+export const IconLink = (props: Props) => {
+  const [iconLinkProps, aTagProps] = splitProps(props, ["name", "label"])
+
+  return (
+    <Tooltip.Root closeDelay={150} openDelay={300}>
+      <Tooltip.Trigger>
+        <a {...aTagProps}>
+          <Icon name={iconLinkProps.name} class="h-6 w-6 text-fg-muted transition hover:text-fg-default" />
+        </a>
+      </Tooltip.Trigger>
+      <Tooltip.Positioner>
+        <Tooltip.Arrow>
+          <Tooltip.ArrowTip />
+        </Tooltip.Arrow>
+        <Tooltip.Content>{iconLinkProps.label}</Tooltip.Content>
+      </Tooltip.Positioner>
+    </Tooltip.Root>
+  )
+}
