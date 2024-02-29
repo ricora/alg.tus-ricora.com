2d2ee94f031a2762ac551952ebf874478411f64e
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-02-29 16:42:36 +0900

    test: remarkInlineCodeのテストを追加

---
+++ src/lib/remark-plugins/remarkInlineCode.test.ts
@@ -0,0 +1,58 @@
+import { beforeAll, describe, expect, test } from "bun:test"
+import dedent from "dedent"
+import type * as hast from "hast"
+import { JSDOM } from "jsdom"
+import type * as mdast from "mdast"
+import rehypeStringify from "rehype-stringify"
+import remarkParse from "remark-parse"
+import remarkRehype from "remark-rehype"
+import { unified } from "unified"
+import { remarkInlineCode } from "./remarkInlineCode"
+
+const process = async (md: string) => {
+  let hast: hast.Node
+  let mdast: mdast.Root
+  const html = (
+    await unified()
+      .use(remarkParse)
+      .use(remarkInlineCode)
+      .use(() => (tree: mdast.Root) => {
+        mdast = tree
+        return mdast
+      })
+      .use(remarkRehype)
+      .use(() => (tree: hast.Node) => {
+        hast = tree
+        return hast
+      })
+      .use(rehypeStringify)
+      .process(md)
+  ).toString()
+
+  // @ts-expect-error: hast and mdast is assigned
+  return { hast, mdast, html }
+}
+
+describe("remarkInlineCode", () => {
+  let jsdom: JSDOM
+  let parser: DOMParser
+
+  beforeAll(() => {
+    jsdom = new JSDOM()
+    parser = new jsdom.window.DOMParser()
+  })
+
+  test("a inline code should be wrapped with `inline-code` element", async () => {
+    const md = dedent`
+      This is a paragraph with \`inline code\` in it.
+    `
+
+    const { html } = await process(md)
+    const doc = parser.parseFromString(html, "text/html")
+
+    const inlineCodeElement = doc.querySelector("inline-code")
+    expect(inlineCodeElement).not.toBeNull()
+
+    expect(inlineCodeElement?.textContent).toBe("inline code")
+  })
+})
