dbc62c13d1cb32e418ece93411c026e69a90867b
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-03-07 23:49:01 +0900

    feat: サイトのメタデータを管理するファイルを追加

---
+++ src/lib/metadata.ts
@@ -0,0 +1,6 @@
+// Metadata for the site
+
+export const siteTitle = "RICORA Programming Team"
+export const twitterScreenName = "ricora_alg"
+export const githubScreenName = "ricora"
+export const githubRepositoryUrl = "https://github.com/ricora/alg.tus-ricora.com"
