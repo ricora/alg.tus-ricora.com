3c4c31c5dfa825ffce51d9e68bcdc6b94e506e99
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-03-09 03:33:34 +0900

    chore: Dangerfileを追加

---
+++ dangerfile.ts
@@ -0,0 +1,36 @@
+import { danger, fail, warn } from "danger"
+
+const title = danger.github.pr.title
+const body = danger.github.pr.body
+
+const checkTitle = () => {
+  const allowedPrefixList = ["chore", "docs", "feat", "fix", "perf", "refactor", "style", "test"]
+  const allowTitleRegex = new RegExp(`^(${allowedPrefixList.join("|")})(\\(([\\w$.* -]*)\\))?: (.+)(?:\\n|$)`, "g")
+  if (!allowTitleRegex.test(title)) {
+    for (const prefix of allowedPrefixList) {
+      if (title.startsWith(`${prefix}: `)) {
+        return
+      }
+    }
+    fail(
+      `PRのタイトルが不正です: \`${title}\`\n次のいずれかのPrefixを使用してください: \n${allowedPrefixList
+        .map((prefix) => {
+          return `\`${prefix}: \``
+        })
+        .join(" ")}\n例: \`feat: ユーザーの削除機能を追加\``,
+    )
+  }
+}
+
+const checkCloseIssue = () => {
+  if (
+    !/(close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)\s+(#\d+|https:\/\/github\.com\/[\w.-]+\/[\w.-]+\/issues\/\d+)/i.test(
+      body,
+    )
+  ) {
+    warn("このPRが解決するIssueへのリンクを記載してください。\n例: `close #123`")
+  }
+}
+
+checkTitle()
+checkCloseIssue()
