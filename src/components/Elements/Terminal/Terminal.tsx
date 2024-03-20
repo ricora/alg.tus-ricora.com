2333ec1c7a8963f29f7cfa51e7b33511cfe9405e
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-03-20 17:04:32 +0900

    feat: Terminalに装飾を追加
    Co-authored-by: r4ai <96561881+r4ai@users.noreply.github.com>

--- src/components/Elements/Terminal/Terminal.tsx
+++ src/components/Elements/Terminal/Terminal.tsx
@@ -8,5 +8,17 @@ export const Terminal: Component = () => {
     new CustomTerminal(terminalDiv)
   })
 
-  return <div ref={(el) => (terminalDiv = el)}></div>
+  return (
+    <div class="rounded-md bg-black">
+      <div class="relative flex h-7 w-full flex-row items-center justify-center rounded-t-md bg-[#363636]">
+        <div class="absolute left-0 top-0 flex h-full flex-row items-center gap-2 px-3">
+          <div class="size-3 rounded-full bg-[#ED6A5E]" />
+          <div class="size-3 rounded-full bg-[#F4BD50]" />
+          <div class="size-3 rounded-full bg-[#61C454]" />
+        </div>
+        <div class="text-xs text-white/90">guest@alg-blog</div>
+      </div>
+      <div class="*:*:rounded-md *:p-2" ref={(el) => (terminalDiv = el)}></div>
+    </div>
+  )
 }
