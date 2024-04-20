47abcfed9f65bab72e2cc7a8f4b0e28b52738710
rai <mj82xx@gmail.com>
2024-04-21 00:44:06 +0900

    feat: aspect-ratioが指定されているiframeのwidthとheightを100%に変更する

--- src/components/Elements/Embed/OEmbedRich.tsx
+++ src/components/Elements/Embed/OEmbedRich.tsx
@@ -1,7 +1,23 @@
+import { fromHtml } from "hast-util-from-html"
+import { select } from "hast-util-select"
+import { toHtml } from "hast-util-to-html"
 import type { Component } from "solid-js"
 import type { OEmbedRich as OEmbedRichSchema } from "./oEmbedSchema"
 import { sanitize } from "./sanitize"
 
+/**
+ * aspect-ratioが指定されているiframeのwidthとheightを100%に変更する
+ */
+const transform = (html: string) => {
+  const hast = fromHtml(html)
+  const iframe = select("iframe", hast)
+  if (iframe?.properties.style?.toString().includes("aspect-ratio:")) {
+    iframe.properties.width = "100%"
+    iframe.properties.height = "100%"
+  }
+  return toHtml(hast)
+}
+
 export const OEmbedRich: Component<{ oEmbed: OEmbedRichSchema }> = (props) => {
-  return <div class="w-full" innerHTML={sanitize(props.oEmbed.html)} data-oembed />
+  return <div class="w-full" innerHTML={transform(sanitize(props.oEmbed.html))} data-oembed />
 }
