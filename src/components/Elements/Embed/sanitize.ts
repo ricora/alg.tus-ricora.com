727a41645eeeaa0c1eafd28a5f0d8bc9f6a83ac5
rai <mj82xx@gmail.com>
2024-04-20 11:00:12 +0900

    fix: 埋め込みiframeで固定値の高さの指定を無効化

--- src/components/Elements/Embed/sanitize.ts
+++ src/components/Elements/Embed/sanitize.ts
@@ -4,4 +4,5 @@ export const sanitize = (html: string) =>
   DOMPurify.sanitize(html, {
     ADD_TAGS: ["iframe"],
     ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "scrolling"],
+    FORBID_ATTR: ["height", "width"],
   })
