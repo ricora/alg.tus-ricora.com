8fd080ee4f653bad33f4c85633495ad69fffa07c
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-06-16 10:45:36 +0900

    chore: pre-commitの設定をlefthookに移行

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-/** @type {import("lint-staged").Config} */
-export default {
-  "*.{js,cjs,ts,jsx,tsx,astro}": ["eslint --fix", "prettier --write"],
-  "*.{md,mdx,html,css,json,yaml,yml}": ["prettier --write"],
-}
