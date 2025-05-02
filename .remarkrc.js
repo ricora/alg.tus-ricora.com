241cae8c4a7edee75dcb077e21837944450682ee
rai <96561881+r4ai@users.noreply.github.com>
2025-05-03 00:14:56 +0900

    chore: fix broken autofix for url

--- .remarkrc.js
+++ .remarkrc.js
@@ -3,22 +3,45 @@ import remarkPresetLintConsistent from "remark-preset-lint-consistent"
 import remarkPresetLintRecommended from "remark-preset-lint-recommended"
 import remarkFrontmatter from "remark-frontmatter"
 import remarkMath from "remark-math"
-import remarkGfm from "remark-gfm"
 import remarkPresetLintMarkdownStyleGuide from "remark-preset-lint-markdown-style-guide"
 import remarkPresetPrettier from "remark-preset-prettier"
 import remarkLintNoLiteralUrls from "remark-lint-no-literal-urls"
-import remarkCallout from "@r4ai/remark-callout"
+import remarkLintNoUndefinedReferences from "remark-lint-no-undefined-references"
+import remarkLintNoEmphasisAsHeading from "remark-lint-no-emphasis-as-heading"
 
 export default {
   plugins: [
     remarkFrontmatter,
     remarkMath,
-    remarkGfm,
-    remarkCallout,
     remarkPresetLintConsistent,
     remarkPresetLintRecommended,
     remarkPresetLintMarkdownStyleGuide,
     remarkPresetPrettier,
+    [
+      remarkLintNoUndefinedReferences,
+      {
+        allow: [
+          "!note",
+          "!info",
+          "!abstract",
+          "!tldr",
+          "!todo",
+          "!tip",
+          "!success",
+          "!question",
+          "!warning",
+          "!caution",
+          "!failure",
+          "!danger",
+          "!error",
+          "!bug",
+          "!important",
+          "!example",
+          "!quote",
+        ],
+      },
+    ],
+    [remarkLintNoEmphasisAsHeading, false],
     [remarkLintNoLiteralUrls, false], // disable this rule because mdx only supports literal urls
   ],
 }
